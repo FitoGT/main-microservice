292502683dc30b364ba73d3f0b20edc67d5fe515
Adolfo <adolfo.guevara.11@gmail.com>
2022-07-06 20:11:15 -0300

    communicating with the django app

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
-import queue
 import pika
+import json
+from main import Product, db
 
 params = pika.URLParameters(
     "amqps://xjjbbkgb:Vyt7GaMax2oyyBkMiuWUln5OY9HCb0vh@moose.rmq.cloudamqp.com/xjjbbkgb")
@@ -13,10 +14,30 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('recieved in main')
-    print(body)
+    data = json.loads(body)
+    if properties.content_type == 'product_created':
+        product = Product(
+            id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
-channel.basic_consume(queue="main", on_message_callback=callback)
+channel.basic_consume(
+    queue="main", on_message_callback=callback, auto_ack=True)
 
 print("consuming")
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from dataclasses import dataclass
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import requests
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
@@ -10,7 +12,12 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
@@ -24,9 +31,29 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name="user_product_id")
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'Hello'
+    return jsonify(Product.query.all())
+
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://172.17.0.1:8000/api/user')
+    json = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+
+        #event
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message': 'success'
+    })
+
 
 
 if __name__ == '__main__':
